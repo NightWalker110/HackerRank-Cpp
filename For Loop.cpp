f50c2b403084a29a88830bb8c4c23c17daa81c77
Shubham Kumar Singh <61842432+NightWalker110@users.noreply.github.com>
2021-01-03 22:10:31 +0530

    Create For Loop.cpp

---
+++ For Loop.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    string c[]={"","one","two","three","four","five","six","seven","eight","nine"};
+    cin>>a>>b;
+    for(int i=a;i<=b;i++)
+        cout<<((i<=9)?c[i]:((i%2==0)?"even":"odd"))<<endl;
+}
